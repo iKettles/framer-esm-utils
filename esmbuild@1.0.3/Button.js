97ff50b432107a2d291c9bf01950f2433f77aba5
deploy <deploy@users.noreply.github.com>
2021-07-15 14:44:21 +0000

    Updated

---
+++ esmbuild@1.0.3/Button.js
@@ -0,0 +1,30 @@
+// src/Button.tsx
+import {
+  createElement
+} from "react";
+
+// esbuild-css-modules-plugin-namespace:/tmp/tmp-1697-p5PkBYyh1C13/main/src/Button.modules.css.js
+var digest = "4b54cbb1b9be55387935e56c851dbfed8fe8fef6b1f9ac6745cee5f280f24185";
+var css = `._button_kpmle_1 {
+  background-color: #c8d5ff;
+}
+`;
+(function() {
+  if (!document.getElementById(digest)) {
+    var ele = document.createElement("style");
+    ele.id = digest;
+    ele.textContent = css;
+    document.head.appendChild(ele);
+  }
+})();
+var Button_modules_css_default = { "button": "_button_kpmle_1" };
+
+// src/Button.tsx
+function Button({ title = "Title" }) {
+  return /* @__PURE__ */ createElement("button", {
+    className: Button_modules_css_default.button
+  }, title);
+}
+export {
+  Button
+};
---
+++ esmbuild@1.0.3/index.js
@@ -0,0 +1,5 @@
+// src/index.tsx
+import { Button } from "./Button.js";
+export {
+  Button
+};

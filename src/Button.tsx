8b2bf4160a54e8f1f15a7bf43482d2bd1b669f46
Iain Kettles <ikettles@gmail.com>
2021-07-21 12:16:30 +0200

    Declare property controls in module

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,28 @@
 import * as React from "react";
+// @ts-ignore
 import styles from "./Button.modules.css";
 
-export function Button({ title = "Title" }) {
-  return <button className={styles.button}>{title}</button>;
+// @ts-ignore
+const { addPropertyControls, ControlType } = window.Framer;
+
+interface Props {
+  title: string;
 }
+
+const InnerButton: React.FC<Props> = (props) => {
+  return <button className={styles.button}>{props.title}</button>;
+};
+
+export const Button = InnerButton;
+
+addPropertyControls(Button, {
+  title: {
+    type: ControlType.String,
+  },
+  foo: {
+    type: ControlType.String,
+  },
+  bar: {
+    type: ControlType.String,
+  },
+});
